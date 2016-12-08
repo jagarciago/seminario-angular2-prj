a485d6d7222d40b07fd3a2f78d7d6ab6da05a6e8
jagarciago <alex.garciag@outlook.com>
2016-12-08 13:35:42 -0500

    incluir materialize y crear componente

---
+++ src/app/disc-crear-pedido/disc-crear-pedido.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-disc-crear-pedido',
+  templateUrl: './disc-crear-pedido.component.html',
+  styleUrls: ['./disc-crear-pedido.component.css']
+})
+export class DiscCrearPedidoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
